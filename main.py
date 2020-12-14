9b68526e01c43d7b92f8f47943c2f5579ff8d37f
madoht <oht222@naver.com>
2020-12-15 04:00:01 +0900

    tkinter로 대부분 제작 완료

--- main.py
+++ main.py
@@ -34,44 +34,49 @@ class LoginPage(Frame):
 
         login_button = Button(self, text="로그인", command=lambda: login(
             inputPK, inputID, inputPassword))
-        profile_button = Button(
-            self, text="프로필", command=lambda: master.switch_frame(BalancePage))
         sign_up_button = Button(
             self, text="회원가입", command=lambda: master.switch_frame(SignUpPage))
+        exit_button = Button(self, text="종료", command=self.quit)
 
         PK.grid(row=0, column=1)
         ID.grid(row=1, column=1)
         password.grid(row=2, column=1)
         login_button.grid(row=2, column=2, sticky=W, pady=4)
-        profile_button.grid(row=3, column=2, sticky=W, pady=4)
-        sign_up_button.grid(row=4, column=2, sticky=W, pady=4)
-
-
-class BalancePage(Frame):
-    def __init__(self, master):
-        Frame.__init__(self, master)
-        InputWithdraw = StringVar()
-        InputDeposit = StringVar()
-
-        Label(self, fg="black", text="이름").grid(row=0)
-        Label(self, fg="blue", text="").grid(row=0, column=1)
-        Label(self, fg="black", text="잔액").grid(row=1)
-        Label(self, fg="blue", text="").grid(row=1, column=1)
-
-        withdraw_button = Button(
-            self, text="송금", command=lambda: withdraw(InputWithdraw))
-        withdraw_entry = Entry(self, width=10, textvariable=InputWithdraw)
-
-        deposit_button = Button(
-            self, text="입금", command=lambda: deposit(InputDeposit))
-        deposit_entry = Entry(self, width=10, textvariable=InputDeposit)
-
-        end_button = Button(self, text="종료", command=lambda: )
+        sign_up_button.grid(row=3, column=2, sticky=W, pady=4)
+        exit_button.grid(row=4, column=2, sticky=W, pady=4)
 
 
 class SignUpPage(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
+        inputPK = StringVar()
+        inputUsername = StringVar()
+        inputID = StringVar()
+        inputPassword = StringVar()
+
+        SignInLabel = Label(
+            self, fg="blue", text="회원 가입").grid(row=0, column=1)
+        Label(self, fg="blue", text="보안 코드").grid(row=1)
+        Label(self, fg="blue", text="유저 이름").grid(row=2)
+        Label(self, fg="blue", text="아이디").grid(row=3)
+        Label(self, fg="blue", text="비밀번호").grid(row=4)
+
+        PK = Entry(self, width=20, textvariable=inputPK)
+        username = Entry(self, width=20, textvariable=inputUsername)
+        ID = Entry(self, width=20, textvariable=inputID)
+        password = Entry(self, width=20, textvariable=inputPassword)
+
+        submit_button = Button(self, text="회원 가입", command=lambda: sign_in(
+            inputPK, inputUsername, inputID, inputPassword))
+        back_button = Button(
+            self, text="이전으로", command=lambda: master.switch_frame(LoginPage))
+
+        PK.grid(row=1, column=1)
+        username.grid(row=2, column=1)
+        ID.grid(row=3, column=1)
+        password.grid(row=4, column=1)
+        submit_button.grid(row=10, column=2, sticky=W, pady=4)
+        back_button.grid(row=10)
 
 
 def write(pk, username, id, password):
@@ -106,29 +111,24 @@ def sign_in(pk, username, id, password):
     pass
 
 
-def login(inputPK,
-          inputID,
-          inputPassword):
+def login(inputPK, inputID, inputPassword):
     pk = inputPK.get()
     ID = inputID.get()
     Password = inputPassword.get()
-    db = loginRead(pk)
-    db_id = db[0]
-    db_password = db[1]
-    if db_id != ID or db_password != Password:
-        messagebox.askyesno(title="로그인 실패", message="아이디 혹은 비밀번호가\n 틀렸습니다.")
+    print(type(pk))
+    print(pk)
+    if not pk or not ID or not Password:
+        messagebox.askyesno(
+            title="로그인 에러", message="보안번호, 아이디 혹은 비밀번호를\n 입력하시지않았습니다.")
     else:
-        messagebox.askyesno(title="로그인 성공", message="프로필 버튼을 누르십시오.")
-
-
-def withdraw(balance, amount):
-    balance -= amount
-    messagebox.askyesno(message=f"귀하의 통장에서 {amount}원이 송금되었습니다.")
-
-
-def deposit(balance, amount):
-    balance += amount
-    messagebox.askyesno(title="송금 성공!", message=f"귀하의 통장에 {amount}원이 입금되었습니다.")
+        db = loginRead(pk)
+        db_id = db[0]
+        db_password = db[1]
+        if db_id != ID or db_password != Password:
+            messagebox.askyesno(
+                title="로그인 실패", message="보안번호, 아이디 혹은 비밀번호가\n 틀렸습니다.")
+        else:
+            messagebox.askyesno(title="로그인 성공", message="창을 닫고 진행하십시오.")
 
 
 window = App()
